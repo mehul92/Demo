f6ef51e4d3b2a4950f782bc3845cd90b8f140c51
Mehul Srivastava <mehul.srivastava@zomato.com>
2021-12-16 19:05:04 +0530

    Initial Commit

---
+++ Shared/SomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeApp.swift
+//  Shared
+//
+//  Created by Mehul Srivastava on 16/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
